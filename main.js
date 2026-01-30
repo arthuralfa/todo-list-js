9193075e6a956cd99ac9511df1e98624273cc813
Arthur <arthuralfredo01@gmail.com>
2026-01-29 23:14:41 -0300

    feat: add task object factory

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ const textInput = document.querySelector("#textInput");
 const list = document.querySelector("#list");
 const errorMessage = document.querySelector("#error-message");
 const card = document.querySelector(".card")
+const noTaskMsg = document.querySelector("#no-tasks-msg")
+
+function criarTarefa(mensagem) {
+    return {
+        id: Date.now(),
+        texto: mensagem,
+        concluida: false
+    }
+}
 
 addButton.addEventListener('click', (e) => {
 
@@ -15,8 +24,9 @@ addButton.addEventListener('click', (e) => {
         return;
     }
 
-    card.style.display = "block";
+    noTaskMsg.style.display = "none";
     errorMessage.style.display = "none";
+
 
     const task = document.createElement('li');
     const span = document.createElement('span');
@@ -39,4 +49,13 @@ addButton.addEventListener('click', (e) => {
 
     list.appendChild(task);
     textInput.value = ""
+
+    doneBtn.addEventListener('click', (e) => {
+        task.classList.toggle("done");
+    })
+    deleteBtn.addEventListener('click', (e) => {
+        task.remove();
+        return;
+    })
 })
+
